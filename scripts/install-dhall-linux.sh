8b0b456a0140985c74b8f5eac20b00fda40855e6
Matthew Ostlind <matthew.ostlind@umbrage.com>
2020-11-07 23:56:44 -0600

    copy paste a different thing

--- scripts/install-dhall-linux.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-curl -Lo dhall-json-1.7.3-x86_64-linux.tar.bz2 https://github.com/dhall-lang/dhall-haskell/releases/download/1.36.0/dhall-json-1.7.3-x86_64-linux.tar.bz2
-tar --extract --bzip2 --file dhall-json-*-x86_64-linux.tar.bz2
-sudo cp ./bin/dhall-to-{json,yaml} /usr/local/bin
