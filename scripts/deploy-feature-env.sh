a920cffbf11351420916c415938e31267b0af164
Matthew Ostlind <matthew.ostlind@umbrage.com>
2020-11-13 22:43:26 -0600

    cache the right file

--- scripts/deploy-feature-env.sh
+++ scripts/deploy-feature-env.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-docker images -a
-
 ./k8s/generate-environment-config/feature.sh ./k8s $1
 
 BASE_HOST=$(cat ./k8s/generated/baseHost) skaffold run --default-repo registry.digitalocean.com/mostlind -p prod
