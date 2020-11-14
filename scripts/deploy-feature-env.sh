901f5301e927c312d2e1ede1521737c45c1542b5
Matthew Ostlind <matthew.ostlind@umbrage.com>
2020-11-13 21:31:46 -0600

    list images in script

--- scripts/deploy-feature-env.sh
+++ scripts/deploy-feature-env.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+docker images -a
+
 ./k8s/generate-environment-config/feature.sh ./k8s $1
 
 BASE_HOST=$(cat ./k8s/generated/baseHost) skaffold run --default-repo registry.digitalocean.com/mostlind -p prod
