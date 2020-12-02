5af9e2709ee9b24c67035307adcd8b587592a31a
mostlind <mostlind@gmail.com>
2020-12-01 21:30:25 -0600

    Initial style for card component (#10)
    * Initial card styling and story
    
    * move style to seperate file
    
    * Rename card style file
    
    Co-authored-by: Matthew Ostlind <matthew.ostlind@umbrage.com>

--- frontend/src/components/card/card.stories.tsx
+++ frontend/src/components/card/card.stories.tsx
@@ -1,19 +1,32 @@
 import { Meta } from "@storybook/react/types-6-0";
 import React from "react";
 
-import { Card } from "./card";
+import { Card, CardProps } from "./card";
 
 export default {
   title: "Components/Card",
   component: Card,
   argTypes: {
     backgroundColor: { control: "color" },
+    hoverColor: { control: "color" },
   },
 } as Meta;
 
 /**
  * Primary UI component for user interaction
  */
-export function Normal() {
-  return <Card />;
+export function Normal({
+  mainText = "What is the answer to this question?",
+  subText = "Question 1",
+  backgroundColor = "#41ffcc",
+  hoverColor = "#8effe1",
+}: CardProps) {
+  return (
+    <Card
+      mainText={mainText}
+      subText={subText}
+      backgroundColor={backgroundColor}
+      hoverColor={hoverColor}
+    />
+  );
 }
--- frontend/src/components/card/card.tsx
+++ frontend/src/components/card/card.tsx
@@ -1,10 +1,39 @@
 import React from "react";
+import { spacing } from "../../styles/theme";
+import {
+  Container,
+  TextContainer,
+  MainText,
+  SubText,
+  Circle,
+} from "./card.style";
 
-interface CardProps {
+export interface CardProps {
   mainText: string;
   subText: string;
+  backgroundColor: string;
+  hoverColor: string;
 }
 
-export function Card() {
-  return <h1>hey</h1>;
+export function Card({
+  mainText,
+  subText,
+  backgroundColor,
+  hoverColor,
+}: CardProps) {
+  return (
+    <Container
+      space={spacing.medium}
+      justifyContent="space-between"
+      alignItems="center"
+      backgroundColor={backgroundColor}
+      hoverColor={hoverColor}
+    >
+      <TextContainer space={spacing.extraSmall} direction="vertical">
+        <MainText>{mainText}</MainText>
+        <SubText>{subText}</SubText>
+      </TextContainer>
+      <Circle>&raquo;</Circle>
+    </Container>
+  );
 }
